cc955173827b4c3de7718ea601de411df2dc643f
TheSummerGhost <kiranbharadia@gmail.com>
2023-03-20 18:06:39 +0000

    Added Jumping and Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,37 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private Animator anim;
+
     private float movementInputDirection;
+
+    private int amountOfJumpsLeft;
 
     public float movementSpeed = 10;
 
     public float jumpForce = 16.0f;
 
+    public Transform groundCheck;
+
     private bool isFacingRight = true;
+    private bool isWalking;
+    private bool isGrounded;
+    private bool canJump;
+
+    public int amountOfJumps = 1;
+
+    public float groundCheckRadius;
+
+    public LayerMask whatIsGround;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     // Update is called once per frame
@@ -26,11 +45,28 @@ public class PlayerController : MonoBehaviour
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurroundings();
+        Debug.Log(canJump);
+        Debug.Log(rb.velocity.y);
+    }
+
+    private void CheckIfCanJump() {
+        if (isGrounded && rb.velocity.y <= -0.5) {
+            amountOfJumpsLeft = amountOfJumps;
+        } 
+
+        if (amountOfJumpsLeft <= 0 ) {
+            canJump = false;
+        } else {
+            canJump = true;
+        }
     }
 
     private void CheckInput() {
@@ -38,6 +74,10 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump")) {
             Jump();
         }
+    }
+
+    private void UpdateAnimations() {
+        anim.SetBool("isWalking", isWalking);
     }
 
     private void ApplyMovement() {
@@ -55,9 +95,27 @@ public class PlayerController : MonoBehaviour
         } else if(!isFacingRight && movementInputDirection > 0) {
             Flip();
         }
+
+        if (rb.velocity.x != 0) {
+            isWalking = true;
+        } else {
+            isWalking = false;
+        }
     }
 
     private void Jump() {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (canJump) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
+
+    }
+
+    private void CheckSurroundings() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
