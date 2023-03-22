e95de5708d5abd814c0d67c1c01250933ca87401
TheSummerGhost <kiranbharadia@gmail.com>
2023-03-22 02:23:26 +0000

    remaking enemy behaviour with finite state machine

---
+++ Assets/Scripts/Enemies/StateMachine/Entity.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public D_Entity entityData;
+    public FiniteStateMachine stateMachine;
+    public int facingDirection { get; private set; }
+
+    public Rigidbody2D rb { get; private set; }
+
+    public Animator anim { get; private set; }
+
+    public GameObject aliveGO { get; private set; }
+
+    [SerializeField] private Transform wallCheck, ledgeCheck;
+
+
+    private Vector2 velocityWorkSpace;
+
+    public virtual void Start()
+    {
+        aliveGO = transform.Find("Alive").gameObject;
+        rb = aliveGO.GetComponent<Rigidbody2D>();
+        anim = aliveGO.GetComponent<Animator>();
+
+        stateMachine = new FiniteStateMachine();
+    }
+
+    public virtual void Update()
+    {
+        {
+            stateMachine.currentState.LogicUpdate();
+        }
+    }
+
+    public virtual void FixedUpdate()
+    {
+        stateMachine.currentState.PhysicsUpdate();
+    }
+
+    public virtual void SetVelocity(float velocity)
+    {
+        velocityWorkSpace.Set(facingDirection * velocity, rb.velocity.y);
+        rb.velocity = velocityWorkSpace;
+    }
+
+    public virtual bool CheckWall()
+    {
+        return Physics2D.Raycast(wallCheck.position, aliveGO.transform.right, entityData.wallCheckDistance, entityData.whatIsGround);
+    }
+
+    public virtual bool CheckLedge()
+    {
+        return Physics2D.Raycast(wallCheck.position, aliveGO.transform.right, entityData.wallCheckDistance, entityData.whatIsGround);
+    }
+
+    public virtual void Flip()
+    {
+        facingDirection *= -1;
+        aliveGO.transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+}
